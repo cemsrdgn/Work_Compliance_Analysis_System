7faa7903b8d3c42ad810476b5c34aa5df1535003
alioznn <ozenali195@gmail.com>
2024-01-02 00:10:08 +0300

    hobbie add class

--- Src/User/Candidate CV/Hobies/Hobies.cpp
+++ Src/User/Candidate CV/Hobies/Hobies.cpp
@@ -1,5 +1,51 @@
 //
 // Created by cemsr on 15.12.2023.
 //
-
 #include "Hobies.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void Hobie::display(){
+
+}
+Hobie::Hobie(int id,int relatedsports,string hobies){
+    this->ID=id;
+    this->RelatedSports=relatedsports;
+    this->Hobies=hobies;
+
+}
+
+void Hobie::add(int id,int relatedsports,string hobies){
+    Hobie newhobi(id,relatedsports,hobies);
+    hobie.push_back(newhobi);
+}
+void Hobie::remove(int id,int relatedsports,string hobies){
+    Hobie newhobi(id,relatedsports,hobies);
+    hobie.pop_back();
+}
+
+void Hobie::update() {
+
+    string newHobie;
+    cout << "Enter your new hobie:";
+    getline(cin,newHobie);
+    setHobies(newHobie);
+}
+void Hobie::setHobie(const vector<Hobie> &hobie) {
+    Hobie::hobie = hobie;
+}
+
+int Hobie::getRelatedsports() const {
+    return RelatedSports;
+}
+
+void Hobie::setRelatedsports(int relatedsports) {
+    Hobie::RelatedSports = relatedsports;
+}
+
+const string &Hobie::getHobies() const {
+    return Hobies;
+};
+
--- Src/User/Candidate CV/Hobies/Hobies.h
+++ Src/User/Candidate CV/Hobies/Hobies.h
@@ -6,9 +6,39 @@
 #define WORK_COMPLIANCE_ANALYSIS_SYSTEM_HOBIES_H
 
 
-class Hobies {
+#include <string>
+#include <vector>
+#include "Candidate/User.h"
+class Hobie: public User {
+private:
+    vector<Hobie> hobie;
+    int RelatedSports;
+    string Hobies;
 
+public:
+    Hobie();
+
+    Hobie(int id,int relatedsports,string hobies);
+
+    void display () override;
+    void update();
+    void remove();
+    void add(int id,int relatedsports,string hobies);
+    void remove(int id,int relatedsports,string hobies);
+    const string &get() const;
+
+    void setHobies(const string &Hobies);
+    void add(int id);
+
+    const vector<Hobie> &getHobie() const;
+
+    void setHobie(const vector<Hobie> &hobie);
+
+    int getRelatedsports() const;
+
+    void setRelatedsports(int relatedsports);
+
+    const string &getHobies() const;
 };
-
 
 #endif //WORK_COMPLIANCE_ANALYSIS_SYSTEM_HOBIES_H
