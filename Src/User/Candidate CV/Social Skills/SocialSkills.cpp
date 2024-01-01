70f449ba556eba2c13b63423cf488d003c0c17d4
alioznn <ozenali195@gmail.com>
2024-01-02 00:12:46 +0300

    social skills add class

--- Src/User/Candidate CV/Social Skills/SocialSkills.cpp
+++ Src/User/Candidate CV/Social Skills/SocialSkills.cpp
@@ -1,5 +1,52 @@
 //
 // Created by cemsr on 15.12.2023.
 //
+#include "SocialSkills.h"
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
 
+void SocialFactors::display(){
+
+}
+SocialFactors::SocialFactors(int id,string socialfactors,string volunteerwork) {
+
+    this->ID=id;
+    this->SocialFactor=socialfactors;
+    this->volunteerwork=volunteerwork;
+
+}
+
+void SocialFactors::add(int id,string socialfactors,string volunteerwork){
+    SocialFactors newsocial(id,socialfactors,volunteerwork);
+    Social.push_back(newsocial);
+}
+void SocialFactors::remove(int id,string socialfactors,string volunteerwork) {
+    SocialFactors newsocial(id,socialfactors,volunteerwork);
+    SocialFactor.pop_back();
+}
+
+void SocialFactors::update() {
+
+    string newSocial;
+    cout << "Enter your social factors:";
+    getline(cin,newSocial);
+    setSocialfactors(newSocial);
+}
+void SocialFactors::setSocialfactors(const std::string &socialfactors) {
+    SocialFactors::SocialFactor = SocialFactor;
+}
+
+const vector<SocialFactors> &SocialFactors::getSocial() const {
+    return Social;
+}
+
+void SocialFactors::setSocialFactor(const string &socialFactor) {
+    SocialFactor = socialFactor;
+}
+
+void SocialFactors::setSocial(const vector<SocialFactors> &social) {
+    Social = social;
+}
 #include "SocialSkills.h"
--- Src/User/Candidate CV/Social Skills/SocialSkills.h
+++ Src/User/Candidate CV/Social Skills/SocialSkills.h
@@ -5,10 +5,46 @@
 #ifndef WORK_COMPLIANCE_ANALYSIS_SYSTEM_SOCIALSKILLS_H
 #define WORK_COMPLIANCE_ANALYSIS_SYSTEM_SOCIALSKILLS_H
 
+#include <string>
+#include <vector>
+#include "Candidate/User.h"
+class SocialFactors: public User {
+private:
+    string SocialFactor;
+    vector<SocialFactors> Social;
+    string volunteerwork;
+public:
 
-class SocialSkills {
+    //default constructor
+    SocialFactors();
 
+    // parametreli constructor
+    SocialFactors(int id,string socialfactors,string volunteerwork);
+
+    void display () override;
+    void update();
+    void remove(int id,string socialfactors,string volunteerwork);
+    void add(int id,string volunteerwork,string socialfactors);
+
+    const vector<SocialFactors> &getSocialFactor() const;
+
+    void setSocialFactor(const vector<SocialFactors> &socialFactor);
+
+    const string &getSocialfactors() const;
+
+    void setSocialfactors(const string &socialfactors);
+
+    const string &getVolunteerwork() const;
+
+    void setVolunteerwork(const string &volunteerwork);
+
+    const vector<SocialFactors> &getSocial() const;
+
+    void setSocialFactor(const string &socialFactor);
+
+    void setSocial(const vector<SocialFactors> &social);
 };
+
 
 
 #endif //WORK_COMPLIANCE_ANALYSIS_SYSTEM_SOCIALSKILLS_H
