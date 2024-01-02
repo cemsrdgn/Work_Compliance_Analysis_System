a9b6a94fc19ccc474e96687c71c6698b850482b9
Onur Guven <onurguvenvtn@gmail.com>
2024-01-02 03:46:04 +0300

    personal ve expertisearea eklendi.

--- Src/User/Candidate CV/Expertise Areas/ExpertiseAreas.cpp
+++ Src/User/Candidate CV/Expertise Areas/ExpertiseAreas.cpp
@@ -3,3 +3,46 @@
 //
 
 #include "ExpertiseAreas.h"
+#include <iostream>
+using namespace std;
+
+// Static vector definition
+vector<ExpertiseArea> ExpertiseArea::areas;
+
+// Default constructor
+ExpertiseArea::ExpertiseArea() : areaName(""), description("") {}
+
+// Parametrized constructor
+ExpertiseArea::ExpertiseArea(const string& areaName, const string& description)
+        : areaName(areaName), description(description) {}
+
+// Destructor
+ExpertiseArea::~ExpertiseArea() {
+}
+
+// Display function
+void ExpertiseArea::display() const {
+    cout << "Area Name: " << areaName << endl;
+    cout << "Description: " << description << endl;
+}
+
+// Update function
+void ExpertiseArea::update(const string& areaName, const string& description) {
+    this->areaName = areaName;
+    this->description = description;
+}
+
+// Add new ExpertiseArea to the static vector
+void ExpertiseArea::addArea(const string& areaName, const string& description) {
+    ExpertiseArea newArea(areaName, description);
+    areas.push_back(newArea);
+}
+
+// Static function to display all areas
+void ExpertiseArea::displayAllAreas() {
+    for (size_t i = 0; i < areas.size(); i++) {
+        areas[i].display();
+        cout << endl;
+    }
+}
+
--- Src/User/Candidate CV/Expertise Areas/ExpertiseAreas.h
+++ Src/User/Candidate CV/Expertise Areas/ExpertiseAreas.h
@@ -5,8 +5,72 @@
 #ifndef WORK_COMPLIANCE_ANALYSIS_SYSTEM_EXPERTISEAREAS_H
 #define WORK_COMPLIANCE_ANALYSIS_SYSTEM_EXPERTISEAREAS_H
 
+#include <string>
+#include <vector>
+using namespace std;
+
 
 class ExpertiseAreas {
+public:
+
+    ExpertiseArea
+
+            Expertise
+
+    Expert
+    ExpertiseArea();  // Default constructor
+
+    Expertise
+    ExpertiseArea(const string& areaName, const string& description);  // Parametrized constructor
+    ~
+    ~Expertise
+
+    ~Expert
+
+
+    ExpertiseArea();  // Destructor
+
+
+
+
+
+
+    void display() const;
+
+
+    void update(const string& areaName, const string& description);
+
+
+
+
+    static void addArea(const string& areaName, const string& description);
+
+
+    static void displayAllAreas();
+
+private:
+    string areaName;
+    string description;
+
+    string areaName;
+    string description;
+
+
+    string areaName;
+    string description;
+
+    string areaName;
+    string
+
+            string areaName;
+
+
+    string areaName
+
+    string area
+
+
+    static vector<ExpertiseArea> areas;  // Static vector to store expertise area instances
 
 };
 
